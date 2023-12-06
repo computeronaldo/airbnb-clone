0c7336a48c4dc49e156aa4f089680cb794091bb2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-12-06 23:44:39 +0530

    feat: implemented MenuItem and UserMenu components where MenuItem is a child component to UserMenu component

---
+++ app/components/Navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem = (props: MenuItemProps) => {
+  return (
+    <div
+      onClick={props.onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {props.label}
+    </div>
+  );
+};
+
+export default MenuItem;
