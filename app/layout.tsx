e2fedca4692ba3df3bc59b987803bed306c2fe63
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-12-15 20:32:39 +0530

    modify: fetching current user from DB if user is logged in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
-
 import ClientOnly from "./components/ClientOnly";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import RegisterModal from "./components/Modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/Modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -14,18 +15,21 @@ export const metadata: Metadata = {
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
